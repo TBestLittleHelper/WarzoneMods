5da80438c92c5e6c44019b39866741a0562fade5
TBest <TBestLittleHelper@users.noreply.github.com>
2024-08-06 00:38:46 +0200

    Update Client_PresentConfigureUI.lua

--- Group_Chat/Client_PresentConfigureUI.lua
+++ Group_Chat/Client_PresentConfigureUI.lua
@@ -1,3 +1,4 @@
 function Client_PresentConfigureUI(rootParent)
-	UI.CreateLabel(rootParent).SetText("Have fun! Don't be afraid to contact TBest if you need help.")
+    UI.CreateLabel(rootParent).SetText(
+        "Have fun! Don't be afraid to contact TBest if you need help.")
 end
