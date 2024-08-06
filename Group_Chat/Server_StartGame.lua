a62f7246415b8d29a49445521542df543dffc6d2
TBest <TBestLittleHelper@users.noreply.github.com>
2024-08-06 09:59:50 +0200

    Update Server_StartGame.lua

--- Group_Chat/Server_StartGame.lua
+++ Group_Chat/Server_StartGame.lua
@@ -1,34 +1,23 @@
 function Server_StartGame(game, standing)
-    -- Setup publicGameData
 
+    -- Setup publicGameData
     local publicGameData = Mod.PublicGameData
     publicGameData.GameFinalized = false
-    publicGameData.Chat = {}
-    publicGameData.Chat = BroadcastGroupSetup(game)
+    publicGameData.Chat = {BroadcastGroup = BroadcastGroupSetup(game)}
     Mod.PublicGameData = publicGameData
 
     -- Setup playerGameData
-    PlayerGameData = Mod.PlayerGameData
+    local playerGameData = Mod.PlayerGameData
 
     for _, pid in pairs(game.ServerGame.Game.Players) do
-        if (pid.IsAI == false) then
-            if (PlayerGameData[pid.ID] == nil) then
-                print("Doing playerGameDataSetup")
-                PlayerGameDataSetup(game, standing)
-            end
-        end
+        if (pid.IsAI == false) then playerGameData[pid.ID] = {Chat = {}} end
     end
-    Mod.PlayerGameData = PlayerGameData
+    Mod.PlayerGameData = playerGameData
 end
 
-function PlayerGameDataSetup(game, standing)
-    for _, pid in pairs(game.ServerGame.Game.Players) do
-        if (pid.IsAI == false) then PlayerGameData[pid.ID] = {Chat = {}} end
-    end
-end
 function BroadcastGroupSetup(game)
     local BroadcastGroup = {}
-    BroadcastGroup[1] = "When a game ends, all chat messages Will be deleted."
+    BroadcastGroup[1] = "When a game ends, all chat messages will be deleted."
     BroadcastGroup[2] =
         "Note that messages to the server is rate-limited to 5 calls every 5 seconds. Therefore, do not spam chat or group changes: it won't work!"
     BroadcastGroup[3] = "Please report any bugs and feedback to TBest."
