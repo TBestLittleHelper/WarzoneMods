6868936e3c14f377277c165603c6cc0dc48f26ee
TBest <TBestLittleHelper@users.noreply.github.com>
2024-08-08 01:46:23 +0200

    extract some logic to chatData , wip

--- Group_Chat/GameData.lua
+++ Group_Chat/GameData.lua
@@ -1,49 +1,33 @@
 require("ChatData")
 function GameDataSetup(game)
-    -- Make sure Setup can only be run once ( StartDistribution and StartGame may both run)
+    -- Make sure setup can only be run once ( StartDistribution and StartGame may both run)
     if (Mod.PrivateGameData.ChatGroups ~= nil) then return end
 
     local publicGameData = Mod.PublicGameData
-    local playerGameData = Mod.PlayerGameData
     local privateGameData = Mod.PrivateGameData
 
-    local groupID = WL.TickCount()
-
-    local humanPlayers = {};
-    for _, player in pairs(game.ServerGame.Game.Players) do
-        if (player.IsAI == false) then
-            playerGameData[player.ID] = {
-                ChatGroupMember = {
-                    [groupID] = {Name = "test", Color = "#880085"}
-                }
-            }
-            table.insert(humanPlayers, player.ID)
-        end
-    end
-
-    privateGameData = {
-        ChatGroups = {
-            [groupID] = {
-                Name = "System",
-                OwnerID = -1,
-                ChatHistory = {},
-                Color = "#880085",
-                Members = humanPlayers
-            }
-        }
-    }
-    AddMessage(privateGameData.ChatGroups[groupID].ChatHistory, -1,
-               "When a game ends, all chat messages will be deleted.")
-    AddMessage(privateGameData.ChatGroups[groupID].ChatHistory, -1,
-               "Note that messages to the server is rate-limited. Therefore, do not spam chat or group changes: it won't work!")
-    AddMessage(privateGameData.ChatGroups[groupID].ChatHistory, -1,
-               "Please report any bugs and feedback to TBest.")
-
     publicGameData.GameFinalized = false
+    privateGameData.ChatGroups = {}
 
     -- Save to server
     Mod.PublicGameData = publicGameData
-    Mod.PlayerGameData = playerGameData
     Mod.PrivateGameData = privateGameData
+
+    -- Logic to create a System chat group, with all human players
+    local groupID = WL.TickCount()
+    CreateGroup(groupID, "System", -1, "#880085")
+
+    for _, player in pairs(game.ServerGame.Game.Players) do
+        if (player.IsAI == false) then
+            AddPlayerIDToGroup(groupID, player.ID)
+        end
+    end
+
+    AddMessage(groupID, -1,
+               "When a game ends, all chat messages will be deleted.")
+    AddMessage(groupID, -1,
+               "Note that messages to the server is rate-limited. Therefore, do not spam chat or group changes: it won't work!")
+    AddMessage(groupID, -1, "Please report any bugs and feedback to TBest.")
+
 end
 
