480f965cf0c2b44d79c6eb18f41864d3a157b315
TBest <TBestLittleHelper@users.noreply.github.com>
2024-08-07 14:31:16 +0200

    move to using privateGameData for system chat

--- Group_Chat/GameData.lua
+++ Group_Chat/GameData.lua
@@ -1,38 +1,39 @@
 function GameDataSetup(game)
     local publicGameData = Mod.PublicGameData
-    local playerGameData = Mod.PlayerGameData
+    local privateGameData = Mod.PrivateGameData
 
     -- Make sure Setup can only be run once ( StartDistribution and StartGame may both run)
     if (publicGameData.Chat ~= nil) then return end
-    local function broadcastGroupSetup(game)
-        local BroadcastGroup = {}
-        BroadcastGroup[1] =
-            "When a game ends, all chat messages will be deleted."
-        BroadcastGroup[2] =
-            "Note that messages to the server is rate-limited to 5 calls every 5 seconds. Therefore, do not spam chat or group changes: it won't work!"
-        BroadcastGroup[3] = "Please report any bugs and feedback to TBest."
-        BroadcastGroup.NumChat = 3
-        return BroadcastGroup;
-    end
 
-    for _, pid in pairs(game.ServerGame.Game.Players) do
-        if (pid.IsAI == false) then
-            playerGameData[pid.ID] = {
-                Chat = {},
-                Settings = {
-                    AlertUnreadChat = true,
-                    NumPastChat = 10,
-                    MenuSizeX = 550,
-                    MenuSizeY = 490
-                }
-            }
+    local SystemChat = {
+        "When a game ends, all chat messages will be deleted.",
+        "Note that messages to the server is rate-limited to 5 calls every 5 seconds. Therefore, do not spam chat or group changes: it won't work!",
+        "Please report any bugs and feedback to TBest."
+    }
+    local groupID = WL.TickCount()
+    local humanePlayers = {};
+    for _, player in pairs(game.ServerGame.Game.Players) do
+        print(player.ID)
+        if (player.IsAI == false) then
+            table.insert(humanePlayers, player.ID)
         end
     end
 
+    privateGameData = {
+        ChatGroups = {
+            GroupID,
+            OwnerID = -1,
+            ChatHistory = SystemChat,
+            Color = "#880085",
+            Members = humanePlayers
+        }
+    }
+
     publicGameData.GameFinalized = false
-    publicGameData.Chat = {BroadcastGroup = broadcastGroupSetup(game)}
 
     -- Save to server
     Mod.PublicGameData = publicGameData;
-    Mod.PlayerGameData = playerGameData;
+    Mod.PrivateGameData = privateGameData;
+
 end
+
