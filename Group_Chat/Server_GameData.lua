3214a684c54bf17079964500ff7bc5089a52ebd6
TBest <TBestLittleHelper@users.noreply.github.com>
2024-08-16 13:12:45 +0200

    Use AddSystemMessage instead, to not mark it as unread for players

--- Group_Chat/Server_GameData.lua
+++ Group_Chat/Server_GameData.lua
@@ -19,16 +19,23 @@ function GameDataSetup(game)
 
     for _, player in pairs(game.ServerGame.Game.Players) do
         AddPlayerIDToGroup(groupID, player.ID, game)
+    end
+
+    -- Use AddSystemMessage instead, to not mark it as unread for players
+    local function AddSystemMessage(groupID, senderID, chat)
+        local privateGameDate = Mod.PrivateGameData
+        table.insert(privateGameDate.ChatGroups[groupID].ChatHistory,
+                     {SenderID = senderID, Chat = chat})
+        Mod.PrivateGameData = privateGameDate
 
     end
 
-    AddMessage(groupID, -1,
-               "When a game ends, all chat messages will be deleted.", game)
-    AddMessage(groupID, -1,
-               "Note that messages to the server is rate-limited. Therefore, do not spam chat or group changes: it won't work!",
-               game)
-    AddMessage(groupID, -1, "Please report any bugs and feedback to TBest.",
-               game)
+    AddSystemMessage(groupID, -1,
+                     "When a game ends, all chat messages will be deleted.")
+    AddSystemMessage(groupID, -1,
+                     "Note that messages to the server is rate-limited. Therefore, do not spam chat or group changes: it won't work!")
+    AddSystemMessage(groupID, -1,
+                     "Please report any bugs and feedback to TBest.")
 
 end
 
