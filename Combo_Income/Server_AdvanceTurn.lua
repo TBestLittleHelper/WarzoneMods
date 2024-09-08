c15da7920258da90f05b209d9b07ea0caa8da866
TBest <TBestLittleHelper@users.noreply.github.com>
2024-09-08 02:12:48 +0200

    mvp

--- Combo_Income/Server_AdvanceTurn.lua
+++ Combo_Income/Server_AdvanceTurn.lua
@@ -16,16 +16,26 @@ end
 ---@param skipThisOrder fun(modOrderControl: EnumModOrderControl) # Allows you to skip the current order
 ---@param addNewOrder fun(order: GameOrder) # Adds a game order, will be processed before any of the rest of the orders
 function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder,
-                                  addNewOrder) end
+                                  addNewOrder)
+
+    if order.proxyType == "GameOrderAttackTransfer" then
+        if orderResult.IsSuccessful then
+            combo[order.PlayerID] = combo[order.PlayerID] + 1
+        end
+    end
+end
 
 ---Server_AdvanceTurn_End hook
 ---@param game GameServerHook
 ---@param addNewOrder fun(order: GameOrder) # Adds a game order, will be processed before any of the rest of the orders
 function Server_AdvanceTurn_End(game, addNewOrder)
+    local msg = "Additional income from combo score"
+
     ---@param playerID PlayerID
     ---@param comboScore ComboCounter
     for playerID, comboScore in pairs(combo) do
-        WL.IncomeMod.Create(playerID, comboScore,
-                            "Additional income from combo score")
+        local comboIncomeMod = WL.IncomeMod.Create(playerID, comboScore, msg)
+        addNewOrder(WL.GameOrderEvent.Create(playerID, msg, nil, {}, nil,
+                                             {comboIncomeMod}))
     end
 end
