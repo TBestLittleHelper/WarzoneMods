94f39163d8fc0356885384917b5c4fd41cddc194
TBest <TBestLittleHelper@users.noreply.github.com>
2024-09-08 03:35:52 +0200

    implement best and current combo counter

--- Combo_Income/Server_AdvanceTurn.lua
+++ Combo_Income/Server_AdvanceTurn.lua
@@ -1,12 +1,15 @@
----@alias ComboCounter integer  -- Alias for integer representing a combo count
----@type table<PlayerID, ComboCounter>  -- A table where keys are integers and values are PlayerID
+---@alias CurrentCombo integer  -- Current combo count
+---@alias BestCombo integer -- Best combo this round
+---@type table<PlayerID, {current: CurrentCombo, best: BestCombo}>  -- A table where keys are integers and values are PlayerID
 local combo = {}
 
 ---Server_AdvanceTurn_Start hook
 ---@param game GameServerHook
 ---@param addNewOrder fun(order: GameOrder) # Adds a game order, will be processed before any of the rest of the orders
 function Server_AdvanceTurn_Start(game, addNewOrder)
-    for playerID, _ in pairs(game.Game.PlayingPlayers) do combo[playerID] = 0 end
+    for playerID, _ in pairs(game.Game.PlayingPlayers) do
+        combo[playerID] = {current = 0, best = 0}
+    end
 end
 
 ---Server_AdvanceTurn_Order
@@ -20,7 +23,12 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder,
     if order.proxyType == "GameOrderAttackTransfer" then
         ---@cast orderResult GameOrderAttackTransferResult
         if orderResult.IsSuccessful then
-            combo[order.PlayerID] = combo[order.PlayerID] + 1
+            combo[order.PlayerID].current = combo[order.PlayerID].current + 1
+            if combo[order.PlayerID].current > combo[order.PlayerID].best then
+                combo[order.PlayerID].best = combo[order.PlayerID].current
+            end
+        else
+            combo[order.PlayerID].current = 0
         end
     end
 end
@@ -32,9 +40,10 @@ function Server_AdvanceTurn_End(game, addNewOrder)
     local msg = "Additional income from combo score"
 
     ---@param playerID PlayerID
-    ---@param comboScore ComboCounter
+    ---@param comboScore CurrentCombo
     for playerID, comboScore in pairs(combo) do
-        local comboIncomeMod = WL.IncomeMod.Create(playerID, comboScore, msg)
+        local comboIncomeMod = WL.IncomeMod.Create(playerID, comboScore.best,
+        msg)
         ---@cast WL WL
         addNewOrder(WL.GameOrderEvent.Create(playerID, msg, nil, {}, nil,
                                              {comboIncomeMod}))
