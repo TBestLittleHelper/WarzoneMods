4982f977e613979b1b207a77765ff8c48a35aa88
TBest <TBestLittleHelper@users.noreply.github.com>
2024-09-23 11:12:16 +0200

    Create Client_SaveConfigureUI.lua

---
+++ BetterCities/Client_SaveConfigureUI.lua
@@ -0,0 +1,5 @@
+---Client_SaveConfigureUI hook
+---@param alert fun(message: string) # Alert the player that something is wrong, for example, when a setting is not configured correctly. When invoked, cancels the player from saving and returning
+function Client_SaveConfigureUI(alert)
+
+end
