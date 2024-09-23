73e60236771b6dc7971f88149362c51c54ece8f1
TBest <TBestLittleHelper@users.noreply.github.com>
2024-09-23 12:22:36 +0200

    Create types.lua

---
+++ BetterCities/types.lua
@@ -0,0 +1,8 @@
+---@alias settingsName string
+
+---@class settingValue
+---@field box CheckBox | nil # UI CheckBox. Can use isChecked
+---@field number NumberInputField | nil # Numeric value of setting
+
+---@class settingsTable
+---@field [settingsName] settingValue
