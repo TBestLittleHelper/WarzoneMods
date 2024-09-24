56292a2c3810c586a5641803155888db8fcc50e8
TBest <TBestLittleHelper@users.noreply.github.com>
2024-09-24 07:45:17 +0200

    tweak UI

--- BetterCities/Client_PresentConfigureUI.lua
+++ BetterCities/Client_PresentConfigureUI.lua
@@ -1,3 +1,5 @@
+---@diagnostic disable-next-line: unknown-cast-variable
+---@cast UI UI
 ---@type table<settingsName, {box: boxInput, number: numberInput}>  -- A table where keys are integers and values are PlayerID
 ---@diagnostic disable-next-line: undefined-global
 SettingsTable = {}
@@ -6,28 +8,31 @@ SettingsTable = {}
 ---@param rootParent RootParent
 function Client_PresentConfigureUI(rootParent)
 
-    ---@param boxText string
+    ---@param isBox boolean -- true if checkbox. Else NumberInput
+    ---@param inputText string
     ---@param labelText string
     ---@param settingsName settingsName
-    local function addCheckbox(boxText, labelText, settingsName)
-        local box = UI.CreateCheckBox(rootParent).SetText(boxText)
-        UI.CreateLabel(rootParent).SetText(labelText)
-        SettingsTable[settingsName] = {box = box}
+    local function addInput(isBox, labelText, settingsName)
+        local horizontalGroup = UI.CreateHorizontalLayoutGroup(rootParent);
+
+        if (isBox) then
+            local box = UI.CreateCheckBox(horizontalGroup)
+            SettingsTable[settingsName] = {box = box}
+
+        else
+            local number = UI.CreateNumberInputField(horizontalGroup)
+            SettingsTable[settingsName] = {number = number}
+        end
+        UI.CreateLabel(horizontalGroup).SetText(labelText)
     end
 
-    addCheckbox("City Walls", "City Walls", "cityWalls")
-    addCheckbox("Natural city growth", "Natural city growth",
-                "naturalCityGrowth")
-    addCheckbox("Bomb card damages cities", "Bomb card damages cities",
-                "bombCardDamagesCities")
-    addCheckbox("Wastland neutral cities", "Wastland neutral cities",
-                "wastelandNeutralCities")
-    addCheckbox("Extra armies when deploying in a city",
-                "Extra armies when deploying in a city", "extraArmiesInCity")
-    addCheckbox("Deploy orders outside a city is skipped",
-                "Deploy orders outside a city is skipped",
-                "deployOrdersOutsideCitySkipped")
-    addCheckbox("Remove fog from cities",
-                "Cities is always visible to everyone", "unfogCities")
-
+    addInput(false, "City Walls", "cityWalls")
+    addInput(false, "Natural city growth", "naturalCityGrowth")
+    addInput(true, "Max size of a city", "maxCitySize")
+    addInput(false, "Bomb card damages cities", "bombCardDamagesCities")
+    addInput(false, "Wastland neutral cities", "wastelandNeutralCities")
+    addInput(false, "Extra armies when deploying in a city", "extraArmiesInCity")
+    addInput(false, "Deploy orders outside a city is skipped",
+             "deployOrdersOutsideCitySkipped")
+    addInput(false, "Cities are always visible to everyone", "unfogCities")
 end
