bfb750dee677d1c6b3a29652b3cc9b6b244726b9
TBest <TBestLittleHelper@users.noreply.github.com>
2024-09-24 22:27:22 +0200

    tweak unfog and use cityWalls def bonus

--- BetterCities/Server_AdvanceTurn.lua
+++ BetterCities/Server_AdvanceTurn.lua
@@ -4,7 +4,7 @@ require("Fog")
 ---@diagnostic disable-next-line: unknown-cast-variable
 ---@cast WL WL
 ---@diagnostic disable-next-line: unknown-cast-variable
----@cast Mod ModServerHook
+---@cast Mod ModServerHook | ModSettings
 
 ---Server_AdvanceTurn_Start hook
 ---@param game GameServerHook
@@ -18,7 +18,58 @@ function Server_AdvanceTurn_Start(game, addNewOrder) end
 ---@param skipThisOrder fun(modOrderControl: EnumModOrderControl) # Allows you to skip the current order
 ---@param addNewOrder fun(order: GameOrder) # Adds a game order, will be processed before any of the rest of the orders
 function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder,
-                                  addNewOrder) end
+                                  addNewOrder)
+    if (order.proxyType == "GameOrderAttackTransfer") then
+        ---@cast orderResult GameOrderAttackTransferResult
+        ---@cast order GameOrderAttackTransfer
+        if (Mod.PrivateGameData.Cities[order.To]) then
+            -- Don't trust the structures to exsist, or to be bigger then 0
+            if (game.ServerGame.LatestTurnStanding.Territories[order.To]
+                .Structures == nil) then return end
+            if (game.ServerGame.LatestTurnStanding.Territories[order.To]
+                .Structures[WL.StructureType.City] == nil) then
+                return
+            end
+            if (game.ServerGame.LatestTurnStanding.Territories[order.To]
+                .Structures[WL.StructureType.City] > 0) then return end
+
+            local defBonus =
+                game.ServerGame.LatestTurnStanding.Territories[order.To]
+                    .Structures[WL.StructureType.City] *
+                    (Mod.Settings.CityWalls * 0.01)
+            local attackersKilled =
+                orderResult.AttackingArmiesKilled.NumArmies +
+                    orderResult.AttackingArmiesKilled.NumArmies * defBonus
+
+            -- Minimum kill 1 attacking army
+            if (attackersKilled == 0) then
+                -- Max armies lost is equal to actualArmies
+                attackersKilled = 1
+            elseif (orderResult.ActualArmies.NumArmies - attackersKilled < 0) then
+                local extraDmg = attackersKilled -
+                                     orderResult.ActualArmies.NumArmies
+                orderResult.DamageToSpecialUnits =
+                    orderResult.DamageToSpecialUnits + extraDmg
+                attackersKilled = orderResult.ActualArmies.NumArmies
+            else
+                -- round up, always
+                attackersKilled = math.ceil(attackersKilled)
+            end
+            -- Write to GameOrderResult	 (result)
+            local NewAttackingArmiesKilled = WL.Armies.Create(attackersKilled)
+            orderResult.AttackingArmiesKilled = NewAttackingArmiesKilled
+            local msg = "The city has " .. tostring(defBonus * 100) ..
+                            "% fortification bonus"
+
+            addNewOrder(WL.GameOrderEvent.Create(game.ServerGame
+                                                     .LatestTurnStanding
+                                                     .Territories[order.To]
+                                                     .OwnerPlayerID, msg,
+                                                 {order.PlayerID}, nil))
+
+        end
+    end
+end
 
 ---Server_AdvanceTurn_End hook
 ---@param game GameServerHook
@@ -27,55 +78,34 @@ function Server_AdvanceTurn_End(game, addNewOrder)
 
     if (Mod.Settings.UnfogCities) then
         for _, territoryID in pairs(Mod.PrivateGameData.Cities) do
-            if (game.ServerGame.LatestTurnStanding.Territories[territoryID]
-                .NumArmies.SpecialUnits == {}) then
-                -- Create a new unit, if the old one is missing
-                -- For example after combat
-                local unit = CreateFogUnit()
-                local addUnit = WL.TerritoryModification.Create(territoryID)
-                addUnit.AddSpecialUnits = {unit}
-                local orders = {addUnit}
-                addNewOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral, "News report from the city!",
-                                                     {}, orders))
+            local unfogUnitFound = false
+            for _, unit in ipairs(
+                               game.ServerGame.LatestTurnStanding.Territories[territoryID]
+                                   .NumArmies.SpecialUnits) do
+                if (unit.proxyType == "CustomSpecialUnit") then
+                    ---@cast unit CustomSpecialUnit
+                    if (unit.ModID == 758) then
+                        unfogUnitFound = true
+                        print("unfogUnitFound found ", territoryID, " turn ",
+                              game.Game.NumberOfLogicalTurns)
+                        break
+                    end
+                end
             end
+            if (unfogUnitFound) then return end
+
+            print("unfogUnit missing ", territoryID)
+
+            -- Create a new unit, as the old one is missing
+            -- For example after combat
+            local unit = CreateFogUnit()
+            local addUnit = WL.TerritoryModification.Create(territoryID)
+            addUnit.AddSpecialUnits = {unit}
+            local orders = {addUnit}
+            addNewOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral,
+                                                 "News report from the city!",
+                                                 {}, orders))
+
         end
-
     end
-
-   -- for _, territory in pairs(game.ServerGame.LatestTurnStanding.Territories) do
-   --     if (territory.NumArmies.SpecialUnits ~= nil) then
-   --         local SpecialUnits = territory.NumArmies.SpecialUnits
-   --         for key, value in pairs(SpecialUnits) do
-   --             print(value.proxyType)
-   --             print(value.ID)
---
-   --             local removeUnit = WL.TerritoryModification.Create(territory.ID)
-   --             removeUnit.RemoveSpecialUnitsOpt = {value.ID}
-   --             local orders = {removeUnit}
-   --             addNewOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral, "",
-   --                                                  {}, orders))
-   --         end
-   --     end
-   -- end
-
-end
-function Dump(obj)
-    if obj.proxyType ~= nil then
-        DumpProxy(obj)
-    elseif type(obj) == "table" then
-        DumpTable(obj)
-    else
-        print("Dump " .. type(obj))
-    end
-end
-function DumpTable(tbl)
-    for k, v in pairs(tbl) do
-        print("k = " .. tostring(k) .. " (" .. type(k) .. ") " .. " v = " ..
-                  tostring(v) .. " (" .. type(v) .. ")")
-    end
-end
-function DumpProxy(obj)
-    print("type=" .. obj.proxyType .. " readOnly=" .. tostring(obj.readonly) ..
-              " readableKeys=" .. table.concat(obj.readableKeys, ",") ..
-              " writableKeys=" .. table.concat(obj.writableKeys, ","))
 end
