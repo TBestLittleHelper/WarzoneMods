43c3cc39b25fe279ee0585755f92019c83b7c7cf
TBest <TBestLittleHelper@users.noreply.github.com>
2024-09-24 07:51:26 +0200

    Update types.lua

--- BetterCities/types.lua
+++ BetterCities/types.lua
@@ -1,4 +1,14 @@
 ---@alias settingsName string -- Name of the setting
 ---@alias boxInput CheckBox  -- UI CheckBox
 ---@alias numberInput NumberInputField -- UI NumberInput
+
+---@class UiElementWithBox
+---@field isBox boolean  -- When true, the element is a box.
+
+---@class UiElementWithoutBox
+---@field isBox boolean  -- When false, the element is not a box.
+---@field max number     -- Maximum value, only if isBox is false.
+---@field initial number -- Initial value, only if isBox is false.
+
+---@alias configureUiElement UiElementWithBox | UiElementWithoutBox
 -- todo enum for settings names
