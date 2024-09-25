df818916c004bef5096e0aca080ca9acd480cd14
TBest <TBestLittleHelper@users.noreply.github.com>
2024-09-25 10:32:40 +0200

    ugly settings view
    better then nothing

--- BetterCities/Client_PresentSettingsUI.lua
+++ BetterCities/Client_PresentSettingsUI.lua
@@ -1,5 +1,24 @@
+require("ModSettings")
+
 ---@diagnostic disable-next-line: unknown-cast-variable
 ---@cast UI UI
+---@diagnostic disable-next-line: unknown-cast-variable
+---@cast Mod Mod  | ModClientHook  | ModSettings
 ---Client_PresentSettingsUI hook
 ---@param rootParent RootParent
-function Client_PresentSettingsUI(rootParent) end
+function Client_PresentSettingsUI(rootParent)
+    local settings = GetAllModSettings()
+    for modname, config in pairs(settings) do
+        local horizontalGroup = UI.CreateHorizontalLayoutGroup(rootParent);
+
+        if (type(config) == "boolean") then
+            UI.CreateCheckBox(horizontalGroup).SetIsChecked(config)
+                .SetInteractable(false).SetText(modname)
+        else
+            UI.CreateLabel(horizontalGroup).SetText(modname)
+            UI.CreateNumberInputField(horizontalGroup).SetValue(config)
+                .SetInteractable(false)
+        end
+    end
+
+end
