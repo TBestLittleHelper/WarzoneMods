e74e79ebf0b5add742addf98508da68c6dbcee19
TBest <TBestLittleHelper@users.noreply.github.com>
2025-10-02 13:15:53 +0200

    Tweak config

--- RandomTerrain/Server_StartGame.lua
+++ RandomTerrain/Server_StartGame.lua
@@ -1,31 +1,33 @@
 require("Voronoi")
 
-    --TODO config for Voronoi generation 
+--TODO config for Voronoi generation
 
 local config = {
-        SVG_OUTPUT = false,
-        RANDOM_SEED = 1000,
-    }-- Resolution control
-
-local WIDTH, HEIGHT = 3500, 2500 -- Max size of a map
-local NUM_SITES = 500            -- Unused, we can experiment later
+    SVG_OUTPUT = false,
+    RANDOM_SEED = 1000,
+    WIDTH = 4500,
+    HEIGHT = 2500,
+}
 local CELL_SIZE = 100
 
 ---Places structures on the map based on the sites.
 ---@param standing GameStanding
-local function placeStructures(TerritoryStructure, standing)
+local function placeStructures(TerritoryStructure, standing, game)
     for territoryID, territory in pairs(standing.Territories) do
         local site = TerritoryStructure[territoryID]
-        print (site)
-        print(site.customStructureName)
+        print(site)
         if (site ~= nil) then
+            print(site.customStructureName)
             local structure = {}
             local structureType = WL.StructureType.Custom(site.customStructureName)
             structure[structureType] = 1
             territory.Structures = structure
-        else            
-            print("No site found for territoryID: " .. territoryID)
-        end       
+        else
+            print("No site found for territoryID: " ..
+                territoryID ..
+                " x,y :  " ..
+                game.Map.Territories[territoryID].MiddlePointX .. "," .. game.Map.Territories[territoryID].MiddlePointY)
+        end
     end
 end
 
@@ -39,6 +41,6 @@ function Server_StartGame(game, standing)
 
     local TerritoryStructure = GenerateVoronoi(config, game)
     print("GeneratedVoronoi")
-    placeStructures(TerritoryStructure, standing)
+    placeStructures(TerritoryStructure, standing, game)
     print("Placed structures")
 end
