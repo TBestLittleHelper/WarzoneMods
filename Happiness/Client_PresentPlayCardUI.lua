d632e39868eec216c89485ceefd49b0e6b81e9d3
TBest <TBestLittleHelper@users.noreply.github.com>
2025-12-18 13:31:46 +0100

    silence warning

--- Happiness/Client_PresentPlayCardUI.lua
+++ Happiness/Client_PresentPlayCardUI.lua
@@ -7,7 +7,9 @@ function Client_PresentPlayCardUI(game, cardInstance, playCard, closeCardsDialog
 	Game = game;
 
 	--If this dialog is already open, close the previous one. This prevents two copies of it from being open at once which can cause errors due to only saving one instance of TargetTerritoryBtn
+	---@diagnostic disable-next-line: undefined-global
 	if (Close ~= nil) then
+		---@diagnostic disable-next-line: undefined-global
 		Close();
 	end
 	playCard("You do not need to play this card. Happiness effect will still apply", "", WL.TurnPhase.ActiveCardWoreOff);
