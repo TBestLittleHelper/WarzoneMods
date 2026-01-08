530403a6875221591e1de759f350788349ebd300
TBest <TBestLittleHelper@users.noreply.github.com>
2026-01-08 01:18:15 +0100

    Create Client_PresentSettingsUI.lua

---
+++ Advancements/Client_PresentSettingsUI.lua
@@ -0,0 +1,8 @@
+---@param rootParent RootParent
+function Client_PresentSettingsUI(rootParent)
+    ---@type UI UI
+    UI = UI
+    local vert = UI.CreateVerticalLayoutGroup(rootParent)
+    local horz = UI.CreateHorizontalLayoutGroup(vert)
+    UI.CreateLabel(horz).SetText("Advancments can't be customized. Maybe in the future.")
+end
