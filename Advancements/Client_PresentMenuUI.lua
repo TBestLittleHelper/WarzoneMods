22f0d3a903af0c70010b1b37b78e931c25d6864e
TBest <TBestLittleHelper@users.noreply.github.com>
2026-01-10 03:45:10 +0100

    bit UI work

--- Advancements/Client_PresentMenuUI.lua
+++ Advancements/Client_PresentMenuUI.lua
@@ -1,21 +1,42 @@
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close)
+	if (game.Us == nil) then
+		UI.Alert("You can't do anything as a spectator.")
+		return
+	end
+
+	setMaxSize(550, 650)
+	setScrollable(false, true)
+
+	Advancements = Mod.Settings.Advancments
+
 	---@type UI UI
 	UI = UI
-	local vert = UI.CreateVerticalLayoutGroup(rootParent)
-	local horz = UI.CreateHorizontalLayoutGroup(vert)
+	local verticalMainLayout = UI.CreateVerticalLayoutGroup(rootParent)
+	local advancmentButtons = UI.CreateHorizontalLayoutGroup(verticalMainLayout)
+	local advancmentUpgradeArea = UI.CreateHorizontalLayoutGroup(verticalMainLayout)
 
-	UI.CreateButton(horz).SetText("Economy").SetColor("#FFAA00").SetOnClick(function()
-		ShowEconomyMenuUI()
-    end)
-	UI.CreateButton(horz).SetText("Culture").SetColor("FF7E015A").SetOnClick(function()
-		UI.Alert("Not implemented yet.")
-    end)
-	UI.CreateButton(horz).SetText("Armies").SetColor("FFF10505").SetOnClick(function()
-		UI.Alert("Not implemented yet.")
-	end)
+	if Advancements.Economy.Enabled then
+		UI.CreateButton(advancmentButtons).SetText("Economy").SetColor(Advancements.Economy.Color).SetOnClick(function()
+			SelectAdvancment("Economy", advancmentUpgradeArea, Advancements.Economy.Upgrades)
+		end)
+	end
+	if Advancements.Culture.Enabled then
+		UI.CreateButton(advancmentButtons).SetText("Culture").SetColor(Advancements.Culture.Color).SetOnClick(function()
+			SelectAdvancment("Culture", advancmentUpgradeArea, Advancements.Culture.Upgrades)
+		end)
+	end
+	if Advancements.Armies.Enabled then
+		UI.CreateButton(advancmentButtons).SetText("Armies").SetColor(Advancements.Armies.Color).SetOnClick(function()
+			SelectAdvancment("Armies", advancmentUpgradeArea, Advancements.Armies.Upgrades)
+		end)
+	end
 end
 
-ShowEconomyMenuUI = function()
-	--TODO
-	UI.Alert("todo")
+---@type fun(advancment: "Economy" | "Culture" | "Armies", advancmentUpgradeArea, upgrades)
+SelectAdvancment = function(advancment, advancmentUpgradeArea, upgrades)
+	print("Selected Advancment: " .. advancment)
+	for _, upgrade in ipairs(upgrades) do
+		local upgradeLine = UI.CreateHorizontalLayoutGroup(advancmentUpgradeArea)
+		UI.CreateButton(upgradeLine).SetText(upgrade.Name)
+	end
 end
--- Advancements/Client_SaveConfigureUI.lua
+++ Advancements/Client_SaveConfigureUI.lua
@@ -1,12 +1,29 @@
 local Advancments = {
 	Economy = {
+		Color = "#FFF700",
 		Enabled = true,
 		PointsPerIncome = 1,
 		Upgrades = {
 			{ Name = "Buy Stocks",  Description = "Earn 1 extra point for every 5 income you have.",  IncomeThreshold = 5,  PointsPerIncome = 1, Cost = 5 },
 			{ Name = "Build Farms", Description = "Earn 1 extra point for every 10 income you have.", IncomeThreshold = 10, PointsPerIncome = 1, Cost = 10 }
 		},
-	}
+	},
+	Culture = {
+		Color = "#00FFAA",
+		Enabled = true,
+		PointsPerCity = 2,
+		Upgrades = {
+			{ Name = "National Song Competition", Description = "Earn 1 extra point for every 3 cities you own.", CitiesThreshold = 3, PointsPerCity = 1, Cost = 8 },
+		},
+	},
+	Armies = {
+		Color = "#FF00AA",
+		Enabled = true,
+		PointsPerArmy = 1,
+		Upgrades = {
+			{ Name = "Mercenaries", Description = "Every 5 deployed armies comes with 1 extra army.", ArmiesThreshold = 5, DeployBonus = 1, Cost = 12 },
+		},
+	},
 }
 
 function Client_SaveConfigureUI()
