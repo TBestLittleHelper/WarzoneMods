9001b0c010d131cb5c027476588041f0a0f3e32b
TBest <TBestLittleHelper@users.noreply.github.com>
2026-01-11 03:43:59 +0100

    populate server state on game start

---
+++ Advancements/Server_StartGame.lua
@@ -0,0 +1,41 @@
+function Server_StartGame(game, standing)
+	-- Writable copy of Mod.PrivateGameData
+	local privateGameData = Mod.PrivateGameData
+	privateGameData.Advancement = {}
+
+	-- Data structure:
+	--	privateGameData = {
+	--		[playerID] = {
+	--			Economy = { Points = 0 },
+	--			Culture = { Points = 0 },
+	--			Armies  = { Points = 0 }
+	--		},
+	--		[advancmentName] = {
+	--			[upgradeUID] = { UnlockedBy = {} }
+	--		}
+	--	}
+
+
+	for _, player in pairs(game.ServerGame.Game.Players) do
+		privateGameData[player.ID] = privateGameData[player.ID] or {}
+	end
+
+	for advancmentName, advancement in pairs(Mod.Settings.Advancments) do
+		if advancement.Enabled then
+			for _, player in pairs(game.ServerGame.Game.Players) do
+				privateGameData[player.ID][advancmentName] = { Points = 0 }
+			end
+
+			local Upgrades = advancement.Upgrades
+			for _, upgrade in pairs(Upgrades) do
+				privateGameData[advancmentName][upgrade.UID] = {
+					UnlockedBy = {}
+				}
+			end
+		end
+	end
+
+	-- Write back to server Mod object
+	---@diagnostic disable-next-line: inject-field
+	Mod.PrivateGameData = privateGameData
+end
