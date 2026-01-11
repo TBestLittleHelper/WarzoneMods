4f63485e588115520d471c913b6b7a8af2f4d39a
TBest <TBestLittleHelper@users.noreply.github.com>
2026-01-11 03:44:19 +0100

    Add UID's to upgrades

--- Advancements/Client_SaveConfigureUI.lua
+++ Advancements/Client_SaveConfigureUI.lua
@@ -1,11 +1,12 @@
 local Advancments = {
+	-- Economy ID start at 1, Culture at 30, Armies at 60 to avoid ID conflicts
 	Economy = {
 		Color = "#FFF700",
 		Enabled = true,
 		PointsPerIncome = 1,
 		Upgrades = {
-			{ Name = "Buy Stocks",  Description = "Earn 1 extra point for every 5 income you have.",  IncomeThreshold = 5,  PointsPerIncome = 1, Cost = 5 },
-			{ Name = "Build Farms", Description = "Earn 1 extra point for every 10 income you have.", IncomeThreshold = 10, PointsPerIncome = 1, Cost = 10 }
+			{ UID = 1, Name = "Buy Stocks",  Description = "Earn 1 extra point for every 5 income you have.",  IncomeThreshold = 5,  PointsPerIncome = 1, Cost = 5 },
+			{ ID = 2, Name = "Build Farms", Description = "Earn 1 extra point for every 10 income you have.", IncomeThreshold = 10, PointsPerIncome = 1, Cost = 10 }
 		},
 	},
 	Culture = {
@@ -13,7 +14,7 @@ local Advancments = {
 		Enabled = true,
 		PointsPerCity = 2,
 		Upgrades = {
-			{ Name = "National Song Competition", Description = "Earn 1 extra point for every 3 cities you own.", CitiesThreshold = 3, PointsPerCity = 1, Cost = 8 },
+			{ UID = 30, Name = "National Song Competition", Description = "Earn 1 extra point for every 3 cities you own.", CitiesThreshold = 3, PointsPerCity = 1, Cost = 8 },
 		},
 	},
 	Armies = {
@@ -21,7 +22,7 @@ local Advancments = {
 		Enabled = true,
 		PointsPerArmy = 1,
 		Upgrades = {
-			{ Name = "Mercenaries", Description = "Every 5 deployed armies comes with 1 extra army.", ArmiesThreshold = 5, DeployBonus = 1, Cost = 12 },
+			{ UID = 60, Name = "Mercenaries", Description = "Every 5 deployed armies comes with 1 extra army.", ArmiesThreshold = 5, DeployBonus = 1, Cost = 12 },
 		},
 	},
 }
