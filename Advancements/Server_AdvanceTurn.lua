bcf1deb9b6deec7616ac1f8d844de792b0832578
TBest <TBestLittleHelper@users.noreply.github.com>
2026-01-12 21:01:54 +0100

    wip

--- Advancements/Server_AdvanceTurn.lua
+++ Advancements/Server_AdvanceTurn.lua
@@ -1,3 +1,6 @@
+---@type WL WL
+WL=WL
+
 function Server_AdvanceTurn_Start(game, addNewOrder)
 	-- We need to know what advancments happen when
 	-- All points are saved during the end turn hook.
@@ -31,6 +34,31 @@ function Server_AdvanceTurn_Start(game, addNewOrder)
 			end
 		end
 	end
+
+	-- Start of turn advancments run right away
+	for upgradeUID, playerIDs in pairs(ActiveAdvancmentsStart) do
+		if upgradeUID == 61 then -- Spy Network
+			for _, playerID in pairs(playerIDs) do
+				-- Get a random other player
+				local otherPlayers = {}
+				for _, player in pairs(game.ServerGame.Game.PlayingPlayers) do
+					if player.ID ~= playerID then
+						table.insert(otherPlayers, player)
+					end
+				end
+				if #otherPlayers > 0 then
+					local randomIndex = math.random(1, #otherPlayers)
+					local targetPlayer = otherPlayers[randomIndex]
+
+					-- todo display name
+					local message = targetPlayer .. " has Territories and Income"
+					-- Create a report order
+					local spyReportOrder = WL.GameOrderEvent.Create(playerID, message, {})
+					addNewOrder(spyReportOrder)
+				end
+			end
+		end
+	end
 end
 
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
@@ -38,7 +66,42 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 end
 
 function Server_AdvanceTurn_End(game, addNewOrder)
-	PrivateGameData = Mod.PrivateGameData
+	-- For each advancment that triggers at end turn, give points to players who have it unlocked
+
+
+	for
+
+	-- Income Threshold Advancments
+	if ActiveAdvancmentsEnd[1] then
+		local incomeThreshold = Mod.Settings.Advancments.Economy.Upgrades[1].IncomeThreshold
+		local pointsPerIncome = Mod.Settings.Advancments.Economy.Upgrades[1].PointsPerIncome
+		for _, playerID in pairs(ActiveAdvancmentsEnd[1]) do
+			--https://www.warzone.com/wiki/Mod_API_Reference:GamePlayer
+			local income = game.ServerGame.Game.Players[playerID].Income(0, game.ServerGame.LatestTurnStanding, false,
+				false)
+
+			local bonusPoints = math.floor(income / incomeThreshold) * pointsPerIncome
+			PrivateGameData[playerID].Economy.Points = PrivateGameData[playerID].Economy.Points + bonusPoints
+		end
+	end
+	if ActiveAdvancmentsEnd[2] then
+		local incomeThreshold = Mod.Settings.Advancments.Economy.Upgrades[2].IncomeThreshold
+		local pointsPerIncome = Mod.Settings.Advancments.Economy.Upgrades[2].PointsPerIncome
+		for _, playerID in pairs(ActiveAdvancmentsEnd[2]) do
+			--https://www.warzone.com/wiki/Mod_API_Reference:GamePlayer
+			local income = game.ServerGame.Game.Players[playerID].Income(0, game.ServerGame.LatestTurnStanding, false,
+				false)
+
+			local bonusPoints = math.floor(income / incomeThreshold) * pointsPerIncome
+			PrivateGameData[playerID].Economy.Points = PrivateGameData[playerID].Economy.Points + bonusPoints
+		end
+	end
+
+
+
+	-- Write back to server Mod object
+	---@diagnostic disable-next-line: inject-field
+	Mod.PrivateGameData = PrivateGameData
 end
 
 function GetUnlockedByAdvancmentID(advancmentID, privateGameData)
