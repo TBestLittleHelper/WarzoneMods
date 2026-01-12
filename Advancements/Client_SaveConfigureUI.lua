ccec24a1d57faf4e33ba4513f7530fa7f36c1cbd
TBest <TBestLittleHelper@users.noreply.github.com>
2026-01-12 05:20:02 +0100

    define what turn phase and advancment applies

--- Advancements/Client_SaveConfigureUI.lua
+++ Advancements/Client_SaveConfigureUI.lua
@@ -5,8 +5,8 @@ local Advancments = {
 		Enabled = true,
 		PointsPerIncome = 1,
 		Upgrades = {
-			{ UID = 1, Name = "Buy Stocks",  Description = "Earn 1 extra point for every 5 income you have.",  IncomeThreshold = 5,  PointsPerIncome = 1, Cost = 5 },
-			{ UID = 2, Name = "Build Farms", Description = "Earn 1 extra point for every 10 income you have.", IncomeThreshold = 10, PointsPerIncome = 1, Cost = 10 }
+			{ UID = 1, Name = "Buy Stocks",  Description = "Earn 1 extra point for every 5 income you have.",  IncomeThreshold = 5,  PointsPerIncome = 1, Cost = 5, AdvanceTurn = "End" },
+			{ UID = 2, Name = "Build Farms", Description = "Earn 1 extra point for every 10 income you have.", IncomeThreshold = 10, PointsPerIncome = 1, Cost = 10 , AdvanceTurn = "End" },
 		},
 	},
 	Culture = {
@@ -14,7 +14,7 @@ local Advancments = {
 		Enabled = true,
 		PointsPerCity = 2,
 		Upgrades = {
-			{ UID = 30, Name = "National Song Competition", Description = "Earn 1 extra point for every 3 cities you own.", CitiesThreshold = 3, PointsPerCity = 1, Cost = 8 },
+			{ UID = 30, Name = "National Song Competition", Description = "Earn 1 extra point for every 3 cities you own.", CitiesThreshold = 3, PointsPerCity = 1, Cost = 8, AdvanceTurn = "End"},
 		},
 	},
 	Armies = {
@@ -22,7 +22,7 @@ local Advancments = {
 		Enabled = true,
 		PointsPerArmy = 1,
 		Upgrades = {
-			{ UID = 60, Name = "Mercenaries", Description = "Every 5 deployed armies comes with 1 extra army.", ArmiesThreshold = 5, DeployBonus = 1, Cost = 12 },
+			{ UID = 60, Name = "Mercenaries", Description = "Every 5 deployed armies comes with 1 extra army.", ArmiesThreshold = 5, DeployBonus = 1, Cost = 12, AdvanceTurn = "Order"  },
 		},
 	},
 }
