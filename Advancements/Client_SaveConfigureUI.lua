4cd35eeffdfb31af16e68686ce8c919c47fd3d00
TBest <TBestLittleHelper@users.noreply.github.com>
2026-01-12 06:25:12 +0100

    add spy network upgrade

--- Advancements/Client_SaveConfigureUI.lua
+++ Advancements/Client_SaveConfigureUI.lua
@@ -1,4 +1,3 @@
-
 ---@type Advancments
 local Advancments = {
 	-- Economy ID start at 1, Culture at 30, Armies at 60 to avoid ID conflicts
@@ -7,8 +6,8 @@ local Advancments = {
 		Enabled = true,
 		PointsPerIncome = 1,
 		Upgrades = {
-			{ UID = 1, Name = "Trade Stocks",  Description = "Earn 1 extra point for every 5 income you have.",  IncomeThreshold = 5,  PointsPerIncome = 1, Cost = 5, AdvanceTurn = "End" },
-			{ UID = 2, Name = "Industrial Farms", Description = "Earn 1 extra point for every 10 income you have.", IncomeThreshold = 10, PointsPerIncome = 1, Cost = 10 , AdvanceTurn = "End" },
+			{ UID = 1, Name = "Trade Stocks",     Description = "Earn 1 extra point for every 5 income you have.",  IncomeThreshold = 5,  PointsPerIncome = 1, Cost = 5,  AdvanceTurn = "End" },
+			{ UID = 2, Name = "Industrial Farms", Description = "Earn 1 extra point for every 10 income you have.", IncomeThreshold = 10, PointsPerIncome = 1, Cost = 10, AdvanceTurn = "End" },
 		},
 	},
 	Culture = {
@@ -16,7 +15,7 @@ local Advancments = {
 		Enabled = true,
 		PointsPerCity = 2,
 		Upgrades = {
-			{ UID = 30, Name = "National Song Competition", Description = "Earn 1 extra point for every 3 cities you own.", CitiesThreshold = 3, PointsPerCity = 1, Cost = 8, AdvanceTurn = "End"},
+			{ UID = 30, Name = "National Song Competition", Description = "Earn 1 extra point for every 3 cities you own.", CitiesThreshold = 3, PointsPerCity = 1, Cost = 8, AdvanceTurn = "End" },
 		},
 	},
 	Armies = {
@@ -24,7 +23,8 @@ local Advancments = {
 		Enabled = true,
 		PointsPerArmy = 1,
 		Upgrades = {
-			{ UID = 60, Name = "Mercenaries", Description = "Every 5 deployed armies comes with 1 extra army.", ArmiesThreshold = 5, DeployBonus = 1, Cost = 12, AdvanceTurn = "Order"  },
+			{ UID = 60, Name = "Mercenaries", Description = "Every 5 deployed armies comes with 1 extra army.",                     ArmiesThreshold = 5, DeployBonus = 1,      Cost = 12, AdvanceTurn = "Order" },
+			{ UID = 61, Name = "Spy Network", Description = "Get a report of one random players status at the start of your turn.", Cost = 15,           AdvanceTurn = "Start" },
 		},
 	},
 }
