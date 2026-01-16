199b1458a8a32189e8163b10489df3e5e04f1d9f
TBest <TBestLittleHelper@users.noreply.github.com>
2026-01-16 03:44:12 +0100

    Basic setting options

--- Advancements/Client_PresentConfigureUI.lua
+++ Advancements/Client_PresentConfigureUI.lua
@@ -1,7 +1,26 @@
+require("Enums")
+
 ---Client_PresentConfigureUI hook
 ---@param rootParent RootParent
 function Client_PresentConfigureUI(rootParent)
+    local settings = Mod.Settings
+    local function safeEnabled(categoryName)
+        local category = settings and settings.Advancements and settings.Advancements[categoryName]
+        return category and category.Enabled ~= nil and category.Enabled or true
+    end
+
+    local savedEconomyEnabled = safeEnabled(AdvancementType.Economy)
+    local savedCultureEnabled = safeEnabled(AdvancementType.Culture)
+    local savedArmiesEnabled = safeEnabled(AdvancementType.Armies)
+
+
     local vert = UI.CreateVerticalLayoutGroup(rootParent)
     local horz = UI.CreateHorizontalLayoutGroup(vert)
     UI.CreateLabel(horz).SetText("Advancements can't be customized. Maybe in the future.")
+    local EconomyEnabledBox = UI.CreateCheckBox(vert).SetText("Enable Economy Advancements").SetIsChecked(
+        savedEconomyEnabled)
+    local CultureEnabledBox = UI.CreateCheckBox(vert).SetText("Enable Culture Advancements").SetIsChecked(
+        savedCultureEnabled)
+    local ArmiesEnabledBox = UI.CreateCheckBox(vert).SetText("Enable Armies Advancements").SetIsChecked(
+        savedArmiesEnabled)
 end
