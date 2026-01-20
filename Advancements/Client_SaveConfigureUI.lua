33f7933beb22f10ff313c52835c68605f69de77c
TBest <TBestLittleHelper@users.noreply.github.com>
2026-01-20 01:32:55 +0100

    more placeholder upgrades

--- Advancements/Client_SaveConfigureUI.lua
+++ Advancements/Client_SaveConfigureUI.lua
@@ -8,8 +8,11 @@ local Advancements = {
 		Enabled = true,
 		PointsPerIncome = 1,
 		Upgrades = {
-			{ UID = 1, Name = "Trade Stocks",     Description = "Earn 1 extra point for every 5 income you have.",  IncomeThreshold = 5,  PointsPerIncome = 1, Cost = 5,  AdvanceTurn = "End" },
-			{ UID = 2, Name = "Industrial Farms", Description = "Earn 1 extra point for every 10 income you have.", IncomeThreshold = 10, PointsPerIncome = 1, Cost = 10, AdvanceTurn = "End" }, { UID = 3, Name = "Universal Basic Income", Description = "Increase your basic Income by 5", BasicIncome = 5, Cost = 50, AdvanceTurn = "End" }
+			[1] =
+			{ UID = 1, Name = "Trade Stocks", Description = "Earn 1 extra economic point for every 5 income you have.", IncomeThreshold = 5, PointsPerIncome = 1, Cost = 5, AdvanceTurn = AdvanceTurn.End },
+			[2] = { UID = 2, Name = "Industrial Farms", Description = "Earn 1 extra economic point for every 10 income you have.", IncomeThreshold = 10, PointsPerIncome = 1, Cost = 10, AdvanceTurn = AdvanceTurn.End },
+			[3] = { UID = 3, Name = "Universal Basic Income", Description = "Increase your basic Income by 5", BasicIncome = 5, Cost = 25, AdvanceTurn = AdvanceTurn.End },
+			[4] = { UID = 4, Name = "Welfare fund", Description = "Increase your basic Income by 10", BasicIncome = 10, Cost = 50, AdvanceTurn = AdvanceTurn.End }
 		},
 	},
 	Culture = {
@@ -18,7 +21,7 @@ local Advancements = {
 		PointsPerCity = 2,
 		Upgrades = {
 			[30] =
-			{ UID = 30, Name = "National Song Competition", Description = "Earn 1 extra point for every 3 cities you own.", CitiesThreshold = 3, PointsPerCity = 1, Cost = 8, AdvanceTurn = AdvanceTurn.End },
+			{ UID = 30, Name = "National Song Competition", Description = "Earn 1 extra culture point for every 3 cities you own.", CitiesThreshold = 3, PointsPerCity = 1, Cost = 8, AdvanceTurn = AdvanceTurn.End },
 		},
 	},
 	Armies = {
@@ -28,7 +31,9 @@ local Advancements = {
 		Upgrades = {
 			[60] =
 			{ UID = 60, Name = "Mercenaries", Description = "Every 5 deployed armies comes with 1 extra army.", ArmiesThreshold = 5, DeployBonus = 1, Cost = 12, AdvanceTurn = AdvanceTurn.Order },
-			[61] = { UID = 61, Name = "Spy Network", Description = "Get a report of one random players status at the start of your turn.", SpyReports = 1, Cost = 15, AdvanceTurn = AdvanceTurn.Start },
+			[61] = { UID = 61, Name = "Spy Network in Cities", Description = "Remove fog of war from all cities at the start of your turn.", SpyReports = 1, Cost = 15, AdvanceTurn = AdvanceTurn.Start },
+			[62] = { UID = 62, Name = "Combat Expirience", Description = "Earn 1 extra armies point for each succcsessful attack", PointsPerWinningAttack = 1, Cost = 30, AdvanceTurn = AdvanceTurn.Order },
+			[63] = { UID = 63, Name = "Infiltrate Big Armies", Description = "Any big army stack will be visible on the map.", BigArmySize = 20, Cost = 35, AdvanceTurn = AdvanceTurn.Start },
 		},
 	},
 }
