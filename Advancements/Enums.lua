3ecc08aea649122cc62c4dd6078c02d26ebe1a14
TBest <TBestLittleHelper@users.noreply.github.com>
2026-01-25 22:28:15 +0100

    update enums

--- Advancements/Enums.lua
+++ Advancements/Enums.lua
@@ -13,14 +13,21 @@ AdvanceTurn = {
 }
 
 UpgradeUID = {
-	-- Economy
-	TradeStocks      = 1,
-	IndustrialFarms  = 2,
+	-- Economy (1–29)
+	TradeStocks             = 1,
+	IndustrialFarms         = 2,
+	UniversalBasicIncome    = 3,
+	WelfareFund             = 4,
 
-	-- Culture
-	NationalSong     = 30,
+	-- Culture (30–59)
+	NationalSongCompetition = 30,
+	UrbanLife               = 31,
+	PublicEducation         = 32,
+	Sportswashing           = 33,
 
-	-- Armies
-	Mercenaries      = 60,
-	SpyReportsCities = 61,
+	-- Armies (60+)
+	Mercenaries             = 60,
+	SpyReportsCities        = 61,
+	CombatExperience        = 62,
+	InfiltrateBigArmies     = 63,
 }
